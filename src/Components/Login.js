ebbc895a8d1e37a4d050afe82953c2526c55962c
Naman <namankumar62039@gmail.com>
2023-10-30 01:17:45 +0530

    Buld login page with authentication

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header';
+import { CheckValidition } from '../Utils/LoginValidition';
 
 const Login = () => {
+
+  const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMessage, seterrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const SubmitBtnClick = () => {
+    const errMsg = CheckValidition(email.current.value, password.current.value);
+    seterrorMessage(errMsg);
+  }
+
+  const toggleForm = () => {
+    setIsSignIn(!isSignIn);
+  }
+
   return (
     <div>
 
@@ -12,12 +29,16 @@ const Login = () => {
         <img className='w-screen h-screen' src='https://miro.medium.com/v2/resize:fit:1400/format:webp/1*5lyavS59mazOFnb55Z6znQ.png' />
       </div>
 
-      <form className='bg-black bg-opacity-80 text-white h-auto w-96 py-10 px-10 m-5 mx-auto my-auto top-36 right-0 left-0 absolute rounded-lg '>
-        <h1 className='p-2 my-2 font-bold text-3xl'>Sign In</h1>
-        <input type='email' placeholder='Email' className='p-3 my-4 w-72 bg-slate-800 rounded-md' />
-        <input type='password' placeholder='Password' className='p-3 my-4 w-72 bg-slate-800 rounded-md' />
-        <button className='p-3 my-5 bg-red-700 w-72 font-bold rounded-lg '>Sign In</button>
-        <p className='cursor-pointer'>New to Net Show? Sign Up now</p>
+      <form className='bg-black bg-opacity-80 text-white h-auto w-96 py-10 px-10 m-5 mx-auto my-auto top-20 right-0 left-0 absolute rounded-lg ' onSubmit={(e) => (e.preventDefault())}>
+        <h1 className='p-2 my-2 font-bold text-3xl'>{isSignIn ? "Sign In" : "Sign Up"}</h1>
+        {!isSignIn && (<input type='text' placeholder='User Name' className='p-3 my-4 w-72 bg-slate-800 rounded-md' />)}
+        {!isSignIn && (<input type='number' placeholder='Enter Your Phone Number' className='p-3 my-4 w-72 bg-slate-800 rounded-md' />)}
+
+        <input type='email' ref={email} placeholder='Email Address' className='p-3 my-4 w-72 bg-slate-800 rounded-md' />
+        <input type='password' ref={password} placeholder='Password' className='p-3 my-4 w-72 bg-slate-800 rounded-md' />
+        <p className='text-red-600 font-bold py-2'>{errorMessage}</p>
+        <button className='p-3 my-5 bg-red-700 w-72 font-bold rounded-lg ' onClick={SubmitBtnClick}>{isSignIn ? "Sign In" : "Sign Up"}</button>
+        <p className='cursor-pointer py-4' onClick={toggleForm} style={{ color: isSignIn ? "white" : "skyblue" }}>{isSignIn ? "New to Net Show ? Sign Up now" : "Already A User ? Sign In Now "}</p>
       </form>
 
     </div>
---
+++ src/Utils/LoginValidition.js
@@ -0,0 +1,10 @@
+export const CheckValidition = (email, password) => {
+    const isEmailValid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
+    const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/.test(password);
+
+    if(!isEmailValid) return "Your Email is Invalid";
+    if(!isPasswordValid) return "Your Password is Invalid";
+
+    return null;
+
+}
