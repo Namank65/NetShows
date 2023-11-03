abb79e010d877da4b92f94f9083b02f10fe9cbba
Naman <namankumar62039@gmail.com>
2023-11-04 01:20:00 +0530

    called the moviesApi from tmdb

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,2 +1,10 @@
 export const LogInBgImg = "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*5lyavS59mazOFnb55Z6znQ.png";
 export const USER_AVTAR = "https://occ-0-2610-3647.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABY20DrC9-11ewwAs6nfEgb1vrORxRPP9IGmlW1WtKuaLIz8VxCx5NryzDK3_ez064IsBGdXjVUT59G5IRuFdqZlCJCneepU.png?r=229";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTA5MjAxZmRlZDU4NmZmNDAxNTY5NGZmNjgyNDdjZSIsInN1YiI6IjY1NDUzMjFkMWFjMjkyMDBlMTE5YTc0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1IZufgZLUPcpUWBQ8Em2EU7NtHwHZr01tLGt0OD0oY0'
+    }
+  };
--- src/Utils/ReduxStore.js
+++ src/Utils/ReduxStore.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import UserSlice from "./UserSlice";
+import movieReducer from "./MoviesSlice";
 
 const appStore = configureStore({
     reducer: {
-        user: UserSlice
+        user: UserSlice,
+        movies: movieReducer,
     }
 });
 
