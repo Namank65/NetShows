0681d5aa48db81c3b14fc5f2aa181cb32efb229b
souamya <namankumar62039@gmail.com>
2025-01-04 00:02:56 +0530

    fixed the loading component

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,9 +1,17 @@
-import React from 'react'
-import Login from './Login';
-import Browse from './Browse';
+import React, { lazy, Suspense } from 'react'
 import { createBrowserRouter } from 'react-router-dom';
 import { RouterProvider } from 'react-router-dom';
-import VideoFullInfo from './VideoFullInfo';
+// import VideoFullInfo from './VideoFullInfo';
+import Loader from './Loader/Loader';
+// import Login from './Login';
+// import Browse from './Browse';
+
+
+
+const Login = lazy(() => import('./Login'));
+const Browse = lazy(() => import('./Browse'));
+const VideoFullInfo = lazy(() => import('./VideoFullInfo'));
+
 
 const Body = () => {
 
@@ -24,7 +32,9 @@ const Body = () => {
 
     return (
         <div>
+            <Suspense fallback={<Loader/>}>
             <RouterProvider router={appRouter} />
+            </Suspense>
         </div>
     )
 };
---
+++ src/Components/Loader/Loader.js
@@ -0,0 +1,22 @@
+import "./Loader.css"
+
+const Loader = () => {
+  return (
+    <div className="loader">
+
+      <div className="nav-products">
+      <div className="nav"></div>
+      <div className="products"></div>
+      <div className="products"></div>
+      <div className="products"></div>
+      <div className="products"></div>
+      <div className="products"></div>
+      <div className="products"></div>
+      <div className="products"></div>
+      <div className="products"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader;
